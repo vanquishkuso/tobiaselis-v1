0ac176a8c26c154d9a0bb837b66d115c78da3211
Tobias Johansson <tobiyanen@gmail.com>
2021-10-01 01:32:50 +0200

    seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,26 @@
 import * as React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/layout";
 import styled from "styled-components";
 import Banner from "../components/Banner";
-import Projects from "../components/Projects"
+import Projects from "../components/Projects";
 import FirstSection from "../components/FirstSection";
 import Contact from "../components/Contact";
+import SEO from "../components/seo";
 
 const IndexPage = () => {
+  // const data = useStaticQuery(graphql`
+  //   query {
+
+  //   }
+  // `)
   return (
     <Layout>
+      <SEO title="Tobias Elis - Effektiv webbutveckling" description="Effektiva, snabba webbplatser och e-handelswebbplatser. " />
       <Banner />
-      <FirstSection/>
-      <Projects/>
-      <Contact/>
+      <FirstSection />
+      <Projects />
+      <Contact />
     </Layout>
   );
 };
