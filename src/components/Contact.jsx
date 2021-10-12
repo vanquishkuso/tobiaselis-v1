ec54d25a0f78ae38f68212bb6ed2b890ab9825bd
Tobias Johansson <tobiyanen@gmail.com>
2021-10-12 14:36:44 +0200

    mobile menu fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,7 +45,7 @@ const Contact = () => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 600,
+    width: "70%",
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
